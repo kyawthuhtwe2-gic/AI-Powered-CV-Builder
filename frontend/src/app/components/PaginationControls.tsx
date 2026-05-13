bc262a791e3abca9df4fc565da87f0bc019ee775
kyawthuhtwe-gic <kyawthuhtwe@gicjp.com>
2026-05-13 11:02:21 +0630

    Update PaginationControls.tsx

--- frontend/src/app/components/PaginationControls.tsx
+++ frontend/src/app/components/PaginationControls.tsx
@@ -58,7 +58,7 @@ export default function PaginationControls({
         <button
           onClick={goPrev}
           disabled={page <= 0}
-          className={`px-3 py-2 rounded-xl border ${page <= 0
+          className={`px-3 py-1 rounded-xl border ${page <= 0
             ? "text-gray-400 border-gray-200 bg-gray-100"
             : "text-gray-700 border-gray-300 bg-white hover:bg-gray-50"
             }`}
@@ -101,7 +101,7 @@ export default function PaginationControls({
         <button
           onClick={goNext}
           disabled={page >= (totalPages || 1) - 1}
-          className={`px-3 py-2 rounded-xl border ${page >= (totalPages || 1) - 1
+          className={`px-3 py-1 rounded-xl border ${page >= (totalPages || 1) - 1
             ? "text-gray-400 border-gray-200 bg-gray-100"
             : "text-gray-700 border-gray-300 bg-white hover:bg-gray-50"
             }`}
